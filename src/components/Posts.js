2f72ebb22f5e391e41a69fd99c0b4a000da55923
Luke Camelo <azathothbeats@gmail.com>
2018-08-13 16:40:10 -0400

    Mapping over dummy data recieved in fetch request

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,16 +1,27 @@
 import React, { Component } from 'react'
 
 class Posts extends Component {
+  state = {
+    posts: []
+  }
+
   componentDidMount = () => {
     fetch('https://jsonplaceholder.typicode.com/posts')
       .then(res => res.json())
-      .then(json => console.log(json))
+      .then(posts => this.setState({ posts }))
+      .then(() => console.log(this.state.posts))
   }
 
   render() {
+    const postItems = this.state.posts.map(post => (
+      <div key={post.id}>
+        <h3>{post.title}</h3>
+        <p>{post.body}</p>
+      </div>
+    ))
     return (
       <div>
-        <h1>Posts</h1>
+        <h1>{postItems}</h1>
       </div>
     )
   }
