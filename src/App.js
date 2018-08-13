0db7f9bd96498b4850b1919708397bf8763b230d
Luke Camelo <azathothbeats@gmail.com>
2018-08-13 17:06:28 -0400

    Brought in redux, react-redux, react-thunk

--- src/App.js
+++ src/App.js
@@ -1,21 +1,28 @@
 import React, { Component } from 'react'
 import logo from './logo.svg'
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux'
+
 import './App.css'
 
 import Posts from './components/Posts'
 import PostForm from './components/PostForm'
 
+const store = createStore(() => [], {}, applyMiddleware())
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <PostForm />
-        <Posts />
-      </div>
+      <Provider store={store}>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome to React</h1>
+          </header>
+          <PostForm />
+          <Posts />
+        </div>
+      </Provider>
     )
   }
 }
