03ccbdbfeb041c58709408243f0820f0bbd4befb
Luke Camelo <azathothbeats@gmail.com>
2018-08-15 18:29:02 -0400

    Convert UserList component to functional component; destructure props

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -1,23 +1,21 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 
-class UserList extends Component {
-  render() {
-    let list = this.props.users.users.map(user => (
-      <div>
-        <h2>username: {user.username}</h2>
-        <br />
-        <h2>password: {user.password}</h2>
-      </div>
-    ))
-    console.log('UserList render method, props:', this.props.users)
-    return (
-      <div>
-        <h1>hi!</h1>
-        {list}
-      </div>
-    )
-  }
+const UserList = ({ users }) => {
+  let list = users.users.map((user, i) => (
+    <div key={i}>
+      <h2>username: {user.username}</h2>
+      <br />
+      <h2>password: {user.password}</h2>
+    </div>
+  ))
+
+  return (
+    <div>
+      <h1>hi!</h1>
+      {list}
+    </div>
+  )
 }
 
 const mapStateToProps = state => ({
